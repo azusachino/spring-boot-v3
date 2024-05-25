1258f7d102cbc73399fd9fce91dc3c01e5d650bc
haru <azusachino@proton.me>
2024-05-25 13:36:23 +0900

    switch to maven

---
+++ src/test/kotlin/icu/azusachino/spring/archive/GenericTests.kt
@@ -0,0 +1,21 @@
+package icu.azusachino.spring.archive
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+/**
+ *
+ *
+ * @author haru
+ * @date 2024-05-25 13:31
+ */
+
+class GenericTests {
+
+    @Test
+    fun test() {
+        val x = 1;
+        Assertions.fail<Int>();
+    }
+
+}
